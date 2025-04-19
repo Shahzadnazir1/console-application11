d5c0d7edb549c55bde36a928ac26a3f8892cb99e
Shahzadnazir1 <shahzadnazir2021@gmail.com>
2025-04-19 22:36:11 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleApp11;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+       int x = 20;
+         int y = 30;
+        Console.WriteLine(x+y);
+        string fname = "Shahzad";
+        string lname = "Nazir";
+        Console.WriteLine(fname + " " + lname);
+    }
+}
+    
+   
+    
+
+
